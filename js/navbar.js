c10acd5c0526e8c7f1d32d0b548befac6c1831e6
ye hyang <68382133+sylrose@users.noreply.github.com>
2026-07-02 23:52:57 -0400

    abc

--- js/navbar.js
+++ js/navbar.js
@@ -3,22 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
     <ul>  <li><a href="index.html">Home <span style="font-family:EmojiFont;">g
   </span>
 </a>
-      <li><a href="writings.html">Writings+
-</a></li>
-      <li><a href="blog.html">Blog
-</a></li>
-
-      <li><a href="books.html">Books       
-</a></li>
-      <li><a href="politics.html">Politics
-      </a></li>
-      <li><a href="music.html">Music
-</a></li>
-      <li><a href="illust.html">Illust             
-</a></li>
-      <li><a href="photos.html">Photos</a></li>
-      <li><a href="fandom.html">Fandom</a></li>
-      <li><a href="world.html">World</a></li>
+            <li><a href="creations.html">Creations<span style="font-family:EmojiFont;">j</span></a></li>
+            <li><a href="media.html">Media<span style="font-family:EmojiFont;">o</span></a></li>
+            <li><a href="about.html">Webmistress</a></li>
+            <li><a href="blog.html">Blog</a></li>
+      <li><a href="politics.html">Politics </a></li>
   </ul>
   `;
 
